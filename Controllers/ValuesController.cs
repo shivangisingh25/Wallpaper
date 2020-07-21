a0e5d9f5adff45a0dd9b032b243928fdb5fc303d
Shivangi Singh <shivangisingh211@outlook.com>
2020-07-21 17:31:33 +0530

    Add project files.

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wallpaper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/Values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+       
+    }
+}
